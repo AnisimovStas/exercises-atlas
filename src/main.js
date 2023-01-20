590feb558887a2a30226054d8833f51d03684935
Feralbot <staaasyyaaan@gmail.com>
2023-01-20 16:53:05 +0300

    gh-pages

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import headerApp from "./assets/components/header/header.vue";
-import MusclesApp from "./assets/components/Muscles/muscles.vue";
-import footerApp from "./assets/components/footer/footer.vue";
-createApp(App).mount("#App");
-createApp(headerApp).mount("#headerApp");
-createApp(MusclesApp).mount("#MusclesApp");
-createApp(footerApp).mount("#footerApp");
