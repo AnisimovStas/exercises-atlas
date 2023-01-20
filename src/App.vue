34a7d2870964af36b70acdc696329d7e48729604
Feralbot <staaasyyaaan@gmail.com>
2023-01-20 16:43:59 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <style src="./assets/styles/index.css"></style>
-<template>
-  <headerTop />
-  <Muscles />
-  <foooter />
-</template>
+
 
 <script>
-import headerTop from "./assets/components/header/header.vue";
-import Muscles from "./assets/components/Muscles/muscles.vue";
-import foooter from "./assets/components/footer/footer.vue";
+
 
 export default {
-  components: {
-    foooter,
-    headerTop,
-    Muscles,
-  },
-  name: "App",
+    name: "App",
   data() {
     return {};
   },
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
-createApp(App).mount("#app");
+import headerApp from "./assets/components/header/header.vue";
+import MusclesApp from "./assets/components/Muscles/muscles.vue";
+import footerApp from "./assets/components/footer/footer.vue";
+createApp(App).mount("#App");
+createApp(headerApp).mount("#headerApp");
+createApp(MusclesApp).mount("#MusclesApp");
+createApp(footerApp).mount("#footerApp");
