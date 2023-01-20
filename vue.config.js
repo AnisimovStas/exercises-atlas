849a6a0e2fc7cfdcd531322d2949bbec1d84add6
Feralbot <staaasyyaaan@gmail.com>
2023-01-20 16:45:12 +0300

    commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
+
+module.exports = {
+  pluginOptions: {
+    ghPages: {
+      message: "Updates",
+    },
+  },
+};
+module.exports = {
+  publicPath: "/exercises-atlas/",
+};
